52f7b68a594652ef103fb28936f829fe629bb58a
Roony11-1 <eltheasasin@gmail.com>
2025-10-01 09:39:57 -0300

    Actualice cosas 01.10

--- clase01.10.sql
+++ clase01.10.sql
@@ -1,3 +1,5 @@
+DROP TABLE EMP_EMAILS;
+
 CREATE TABLE EMP_EMAILS(nombre VARCHAR2(250),
                         email VARCHAR2(250));
                         
@@ -7,12 +9,12 @@ BEGIN
     INSERT INTO EMP_EMAILS VALUES(P_NOMBRE, P_EMAIL);
 END;
 
-CREATE OR REPLACE FUNCTION FN_GENERAR_CORREO(P_NOMBRE VARCHAR2)
+CREATE OR REPLACE FUNCTION FN_GENERAR_CORREO(P_NOMBRE VARCHAR2, P_ANTIGUEDAD DATE)
 RETURN VARCHAR2
 IS
     V_EMAIL VARCHAR2(250);
 BEGIN
-    V_EMAIL := SUBSTR(P_NOMBRE, 1, 1)||'@gmail.com';
+    V_EMAIL := SUBSTR(P_NOMBRE, 1, 1)||FN_CALCULAR_ANTIGUEDAD(P_ANTIGUEDAD)||'@gmail.com';
 
     RETURN V_EMAIL;
 END FN_GENERAR_CORREO;
@@ -22,9 +24,10 @@ DECLARE
 BEGIN
     FOR REG_EMP IN (
     SELECT 
-        first_name
+        first_name,
+        hire_date
     FROM EMPLOYEES) LOOP
-        V_EMAIL := FN_GENERAR_CORREO(REG_EMP.first_name);
+        V_EMAIL := FN_GENERAR_CORREO(REG_EMP.first_name, REG_EMP.hire_date);
         SP_GUARDAR_CORREO(REG_EMP.first_name, V_EMAIL);
     END LOOP;
 END;
@@ -51,3 +54,36 @@ BEGIN
         DBMS_OUTPUT.PUT_LINE('El empleado: '||REG_EMP.first_name||' ha trabajado '||FN_CALCULAR_ANTIGUEDAD(REG_EMP.hire_date)||' años');
     END LOOP;
 END;
+
+-- crear una funcion que retorne el monto de la comision de un trabajador
+-- si el trabajador no tiene ese valor debe retornar 0, NO USAR NVL
+
+CREATE OR REPLACE FUNCTION FN_MONTO_COMISION(P_ID NUMBER)
+RETURN NUMBER
+IS
+    V_MONTO NUMBER;
+BEGIN
+    SELECT
+        COMMISSION_PCT
+    INTO V_MONTO
+    FROM EMPLOYEES
+    WHERE P_ID = employee_id;
+    
+    IF V_MONTO IS NOT NULL THEN
+        RETURN V_MONTO;
+    ELSE
+        RETURN 0;
+    END IF;
+END FN_MONTO_COMISION;
+
+DECLARE
+BEGIN
+    FOR REG_EMP IN (
+    SELECT
+        employee_id
+    FROM EMPLOYEES)LOOP
+        DBMS_OUTPUT.PUT_LINE('MONTO DE COMISION DE EL EMPLEADO CON ID '||REG_EMP.employee_id||' es '||FN_MONTO_COMISION(REG_EMP.employee_id));
+    END LOOP;
+END;
+
+-- 
